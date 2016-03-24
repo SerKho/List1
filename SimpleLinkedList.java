f0f9d066bcd0ba618daffa2b45ff5cc804e85714
SerKho <homuch@bigmir.net>
2016-03-24 22:41:23 +0200

    Frame_4_2_3

--- SimpleLinkedList.java
+++ SimpleLinkedList.java
@@ -26,4 +26,27 @@ public class SimpleLinkedList {
     public int getSize() {
         return size;
     }
+
+    public class Node {
+        private Object o;
+        private Node node;
+
+        public Node(){}
+
+        public Object getO() {
+            return o;
+        }
+
+        public void setO(Object o) {
+            this.o = o;
+        }
+
+        public Node getNode() {
+            return node;
+        }
+
+        public void setNode(Node node) {
+            this.node = node;
+        }
+    }
 }
