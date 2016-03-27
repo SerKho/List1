0d4c15c4a9546f0bc8c00a3473b7bed0cdf90f63
SerKho <homuch@bigmir.net>
2016-03-27 23:38:10 +0300

    Frame_4_2_15

---
+++ Comparator.java
@@ -0,0 +1,19 @@
+package Lesson_4_2_1;
+
+/**
+ * Created by 777 on 27.03.2016.
+ */
+public class Comparator implements java.util.Comparator{
+
+
+    Comparator(){
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+
+        String s1 = o1.toString();
+        String s2 = o2.toString();
+        return s1.compareTo(s2);
+    }
+}
